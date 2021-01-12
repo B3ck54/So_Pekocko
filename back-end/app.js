ca646a27d09f933b0a6d4ea7a4204caf3edb19aa
b3ck54 <b3ck54@gmail.com>
2021-01-12 23:15:10 +0100

    add route signup and login, bcrypt and reorg architecture

--- back-end/app.js
+++ back-end/app.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
-const app = express();
+// const sauceRoutes = require ('./routes/sauce');
+const userRoutes = require ('./routes/user');
 
-app.use('/api/auth/login', (req, res, next) => {
-    const user = [
-      {
-        _id: '1',
-        email: 'test@email.com',
-        password: 'MotDePasse',
-        userId: '1',
-      },
-    ];
-    res.status(200).json(user);
-  });
+const path = require('path');
+
+const connectionSecurity = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l3xmz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect( connectionSecurity,
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+// app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
